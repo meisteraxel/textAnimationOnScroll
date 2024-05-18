9ba2189750c27f14828bd4fb8f7826524b834f17
Axel <meister.axel@gmail.com>
2024-05-18 11:22:00 +0200

    different tests with scrolling animation

--- app/Components/Paragraph.jsx
+++ app/Components/Paragraph.jsx
@@ -1,59 +1,45 @@
 "use client";
 import { useRef } from "react";
-import { useScroll, motion, useTransform } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
-export const Paragraph = ({ value }) => {
-  const element = useRef(null);
+const Paragraph = () => {
+  const textRef = useRef(null);
   const { scrollYProgress } = useScroll({
-    target: element,
-    offset: ["start 0.5", "start start"],
+    target: textRef,
+    offset: ["10% 90%", "60% 30%"],
   });
 
-  const words = value.split(" ");
+  const words =
+    "We help companies identify product-market fit through user research and iterative testing. By collaborating with internal stakeholders, we solve product and UX challenges by crafting seamless, user-centric solutions and bringing visuals to life from development to launch".split(
+      " "
+    );
 
   return (
-    <p
-      className="text-5xl max-w-5xl p-10 mx-auto relative flex flex-wrap"
-      ref={element}
-    >
-      {words.map((word, i) => {
-        const start = i / words.length;
-        const end = start + 1 / words.length;
-        return (
-          <Word key={i} range={[start, end]} progress={scrollYProgress}>
-            {word}
-          </Word>
-        );
-      })}
-    </p>
+    <>
+      <div
+        ref={textRef}
+        className="text-5xl max-w-5xl p-10 mx-auto relative flex flex-wrap"
+      >
+        {words.map((word, index) => {
+          const wordOpacity = useTransform(
+            scrollYProgress,
+            [index / words.length, (index + 1) / words.length],
+            [0.1, 1]
+          );
+
+          return (
+            <motion.span
+              key={index}
+              className="mr-3 mt-3 transition-opacity"
+              style={{ opacity: wordOpacity }}
+            >
+              {word}{" "}
+            </motion.span>
+          );
+        })}
+      </div>
+    </>
   );
 };
 
-const Word = ({ children, range, progress }) => {
-  const characters = children.split("");
-  const amount = range[1] - range[0];
-  const step = amount / children.length;
-  return (
-    <span className="mr-3 mt-3">
-      {characters.map((character, i) => {
-        const start = range[0] + i * step;
-        const end = range[0] + (i + 1) * step;
-        return (
-          <Character key={i} range={[start, end]} progress={progress}>
-            {character}
-          </Character>
-        );
-      })}
-    </span>
-  );
-};
-
-const Character = ({ children, range, progress }) => {
-  const opacity = useTransform(progress, range, [0.2, 1]);
-
-  return (
-    <motion.span className="transition-opacity" style={{ opacity }}>
-      {children}
-    </motion.span>
-  );
-};
+export default Paragraph;
--- app/page.js
+++ app/page.js
@@ -1,13 +1,10 @@
-import { Paragraph } from "./Components/Paragraph";
-
-const text =
-  "We help companies identify product-market fit through user research and iterative testing. By collaborating with internal stakeholders, we solve product and UX challenges by crafting seamless, user-centric solutions and bringing visuals to life from development to launch.";
+import Paragraph from "./Components/Paragraph";
 
 export default function Home() {
   return (
     <>
       <div className="h-screen"></div>
-      <Paragraph value={text} />
+      <Paragraph />
       <div className="h-screen"></div>
     </>
   );
